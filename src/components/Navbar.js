2d066b789db210fa385195494268acc51a3ca846
atharva030 <atharvajagdale28@gmail.com>
2022-06-26 11:38:47 +0530

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,7 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
       <a className="navbar-brand " href="#">PenText</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+   
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
@@ -33,11 +31,15 @@ export default function Navbar(props) {
         </form>
         
       </div>
-      <div className={`form-check form-switch text-${props.mode==='dark'? 'light':'dark'}`}>
+      <div className={`form-check form-switch text-${props.mode==='dark'? 'light':'dark'} mx-3`}>
   <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
   <label className="form-check-label " htmlFor="flexSwitchCheckDefault" > {props.mode==='dark'? 'Disable':'Enable'} Dark Mode</label>
 </div>
+<button className="navbar-toggler my-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
     </div>
+    
    
   </nav>
   )
